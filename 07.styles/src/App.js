252fd2184206d97525a9b03d0735ac43a6bc11b5
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-06-13 19:48:56 +0700

    Add files via upload

---
+++ 07.styles/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Counter } from './components/counter/Counter';
+import { CounterSass } from './components/counter-sass/Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <CounterSass />
+    </div>
+  );
+}
+
+export default App;
