eff825be5cb2c3aac1535648fb627b32e2bf3763
mida2709 <126259059+mida2709@users.noreply.github.com>
2023-06-13 19:54:53 +0700

    Add files via upload

---
+++ 10.consuming-api/src/config/index.js
@@ -0,0 +1,8 @@
+export const config = {
+    google: {
+      clientId:
+        "607095429719-bdjr9f2lnvs2uvhcc3p7g1oqhhnef0n9.apps.googleusercontent.com",
+    },
+  };
+  
+  export default config;
